8e2aa14ad3f6883691858bb8a95f3351a14c46fd
diptomistry <diptomistrybd@gmail.com>
2023-11-16 03:06:11 +0600

    zdxfc

--- routes/test.js
+++ routes/test.js
@@ -1,8 +1,10 @@
-var express = require("express");
-var router = express.Router();
+var express = require("express");//This line imports the Express.js library, which is a web application framework for Node.js.
 
-router.get("/", function (req, res, next) {
-  res.send("Api is working properly");
+
+var router = express.Router();//This line creates a new router object.
+
+router.get("/", function (req, res, next) {//This line defines a route handler for HTTP GET request to the root path (/).when a get request is made to this url ,the function is called
+  res.send("Api is working properly");//This line sends the string "Api is working properly" as the response.
 });
 
-module.exports = router;
+module.exports = router;//This line exports the router object so that it can be used by other parts of the application by importing it using require().
