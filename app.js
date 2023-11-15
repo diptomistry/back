21f7f2f8cc9fb8318062960206f19f880eafcbb2
diptomistry <diptomistrybd@gmail.com>
2023-11-16 01:04:22 +0600

    fgb

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var testRouter=require('./routes/test');
+var cors=require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,12 +17,14 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/test',testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
