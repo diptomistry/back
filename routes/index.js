bb79088f3edf8a985ea5e551c6f8d3b86dea62f0
diptomistry <diptomistrybd@gmail.com>
2023-11-16 18:51:01 +0600

    gh

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const oracDb = require('oracledb'); 
+
+let conn=oracDb.getConnection(
+  {
+    user: "s2020615644",
+    password: "dpass",
+    connectString: '103.221.254.51:1521/pdbdbms2023.cse.du.ac.bd',
+  }
+)
+console.log("successfully connected to oracle db");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+ router.get('/', function(req, res, next) {
+   res.render('index', { title: 'Express' });
+ });
 
 module.exports = router;
